f76de702b4500a69c39ca8f7c618ad860e11f27c
柳沢 英俊 <yanagisawa-h@gnavi.co.jp>
2017-08-10 15:35:38 +0900

    第３回完成

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
 const vm = new Vue({
-    el: '#app',
-    data() {
-        return {}
+  el: '#app',
+  data() {
+    return {
+      memos: []
+    }
+  },
+  template: `
+    <div>
+      <editor-view @add="add"></editor-view>
+      <list-view :memos="memos" @remove="remove"></list-view>
+    </div>
+  `,
+  methods: {
+    add(newMemo) {
+      newMemo.id = this.nextId
+      this.memos.push(newMemo)
     },
-    template: `
-        <div>
-            <list-item></list-item>
-        </div>
-    `
-}) 
+    remove(id) {
+      const index = this.memos.findIndex((memo) => {
+        return memo.id === id
+      })
+      this.memos.splice(index, 1)
+    }
+  },
+  computed: {
+    nextId() {
+      return this.memos.reduce((id, memo) => {
+          return id < memo.id ? memo.id : id
+        }, 0) + 1
+    }
+  }
+})
--- components/ListItem.js
+++ components/ListItem.js
@@ -1,34 +1,35 @@
 Vue.component('list-item', {
-    data() {
-        return {
-            memo: {
-                id: 1,
-                text: 'テスト',
-                date: '16-10-28',
-                tags: ['タグ1', 'タグ2']
-            }
-        }
-    },
-    template: `
-        <div class="list-item">
-            <div>
-                <span>id</span>:
-                <span>{{memo.id}}</span>
-            </div>
-            <div>
-                <span>text</span>:
-                <span>{{memo.text}}</span>
-            </div>
-            <div>
-                <span>date</span>:
-                <span>{{memo.date}}</span>
-            </div>
-            <div>
-                <span>tags</span>:
-                <span>
-                    <span v-for="tag in memo.tags">{{tag}}</span>
-                </span>
-            </div>
-        </div>
-    `
+  props: {
+    memo: Object
+  },
+  template: `
+    <div class="list-item">
+      <div>
+        <span>id</span>：
+        <span>{{memo.id}}</span>
+      </div>
+      <div>
+        <span>text</span>：
+        <span>{{memo.text}}</span>
+      </div>
+      <div>
+        <span>date</span>：
+        <span>{{memo.date}}</span>  
+      </div>
+      <div>
+        <span>tags</span>：
+        <span>
+          <span v-for="tag in memo.tags">{{tag}}</span>
+        </span>
+      </div>
+      <div>
+        <button @click="remove(memo.id)">削除</button>
+      </div>
+    </div> 
+  `,
+  methods: {
+    remove(id) {
+      this.$parent.$emit('remove', id)
+    }
+  }
 })
